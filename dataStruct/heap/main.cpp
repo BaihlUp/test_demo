701cc2b14d7533d08df6bd203490bffc0f62846d
baihlUp <2677443264@qq.com>
2020-02-17 22:11:47 +0800

    增加堆排序，大顶堆，从小到大排序

--- dataStruct/heap/main.cpp
+++ dataStruct/heap/main.cpp
@@ -60,15 +60,63 @@ public:
         return ret;
     }
 
+    int getSize(){
+        return count;
+    }
+
+    void print(){
+
+        for(int i = 1; i <= count; i++)
+        {
+            cout<< data[i] <<" ";
+        }
+        cout<<endl;
+    }
+
+    void heapify(int i, int n){
+        while(true){
+            int maxpos = i;
+            if((2*i <= n) && (data[i] < data[2*i])) maxpos = 2*i;
+            if((2*i + 1 <= n) && (data[maxpos] < data[2*i +1])) maxpos = 2*i+1;
+            if(maxpos == i)
+                break;
+            swap(data[i], data[maxpos]);
+            i = maxpos;
+        }
+    }
+
+    //建堆
+    void buildheap()
+    {
+        //下标count/2是堆的最后一个不是叶子节点，count/2之后都是叶子节点
+        for(int i = count/2; i > 0; i--)
+            heapify(i, count);
+    }
+
+    void sortheap(){
+        buildheap();
+        print();
+        int k = count;
+        //依此取出堆顶元素，放到数组最后
+        while(k > 1){
+            swap(data[1], data[k]);
+            k--;
+            heapify(1, k);
+        }
+        print();
+    }
+
 };
 
 int main()
 {
     heap<int> maxheap = heap<int>(100);
     srand(time(NULL));
-    for(int i = 0; i < 63; i++)
+    for(int i = 0; i < 30; i++)
         maxheap.insert( rand()%100 );
 
-    cout<<"commplete"<<endl;
+    maxheap.print();
+    maxheap.sortheap();
+
     return 0;
 }
